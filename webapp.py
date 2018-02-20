2d50e0b795b44a9b4045d0585e91d62fc51cb82d
Cameron Squire <camsqu@gmail.com>
2018-02-20 09:22:43 -0800

    opens json file in post function

--- webapp.py
+++ webapp.py
@@ -46,7 +46,8 @@ def home():
 
 @app.route('/posted', methods=['POST'])
 def post():
-    
+    with open('file','r+') as f:
+        data=json.load(f)
     #This function should add the new post to the JSON file of posts and then render home.html and display the posts.
     #Every post should include the username of the poster and text of the post.
 
